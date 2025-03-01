f8e11f0af4fadb2a718a8158142f1292b278ae00
iz00 <isadoracontsost@gmail.com>
2025-03-01 17:34:08 -0300

    Add conversation to bot with /start command
    Implemented start of conversation with bot with /start command.
    Bot requests user to send a YouTube video or playlist URL.
    Allow user to finish conversation with /cancel command.
    ConversationHandler used because it allows for a more complex flow of
    messages between the bot and the user, in states or steps.

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
 import logging
 from os import getenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 BOT_TOKEN = getenv("BOT_TOKEN")
+
+# ConversationHandler states
+URL = range(1)
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -18,10 +28,51 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Triggered by /start command, start conversation with user (get URL and fetch details)."""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Send a YouTube video or playlist URL.\n",
+    )
+
+    return URL
+
+
+# TODO: validate and determine wheter video or playlist
+async def receive_url(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text.replace(" ", "")
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=f"URL received: {url}."
+    )
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Finishes the conversation."""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Process finished. Send /start to begin again.",
+    )
+
+    return ConversationHandler.END
+
+
 if __name__ == "__main__":
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     help_handler = CommandHandler("help", help)
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            URL: [MessageHandler(filters.TEXT & ~filters.COMMAND, receive_url)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
     application.add_handler(help_handler)
+    application.add_handler(conv_handler)
 
     application.run_polling()
