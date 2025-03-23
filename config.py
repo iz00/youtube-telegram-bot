67035f73b3fe83c7cf51b8a305524bd54cf3988d
iz00 <isadoracontsost@gmail.com>
2025-03-23 20:56:57 -0300

    Remove load_dotenv from config.py
    load_dotenv is not necessary in config.py in the implementation,
    since systemd EnvironmentFile will be used to load
    environment variables, instead of .env.

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 # File for configuration variables
 
 from os import getenv
-from dotenv import load_dotenv
-
-# Load environment variables from .env
-load_dotenv()
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY")
